366cbfedc6a362631a2f1d5616fb3c7c6f620b40
StarskyXIII <71606873+StarskyXIII@users.noreply.github.com>
2023-06-13 02:42:10 +0800

    feat: translatable messages

--- src/main/java/com/feywild/quest_giver/entity/QuestVillager.java
+++ src/main/java/com/feywild/quest_giver/entity/QuestVillager.java
@@ -15,7 +15,7 @@ import mods.thecomputerizer.reputation.api.ReputationHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -253,7 +253,7 @@ public class QuestVillager extends Villager {
                         this.interactQuest((ServerPlayer) player, hand);
                         this.setTradingPlayer(player);
                     } else {
-                        (player).sendMessage(new TextComponent("Please be careful not to punch the locals! Please Leave Battle Mode before interacting."), player.getUUID());
+                        (player).sendMessage(new TranslatableComponent("message.quest_giver.punch_locals"), player.getUUID());
                     }
                 }
 
@@ -342,7 +342,7 @@ public class QuestVillager extends Villager {
         if (!stack.isEmpty()) {
             if (QuestData.get(player).checkComplete(GiftTask.INSTANCE, stack)) {
                 if (!player.isCreative()) stack.shrink(1);
-                player.sendMessage(new TextComponent("Thank you, "+ player.getName().getContents() + "!"), player.getUUID());
+                player.sendMessage(new TranslatableComponent("message.quest_giver.accept_gift",player.getName().getContents()), player.getUUID());
                 return true;
             }
         }
